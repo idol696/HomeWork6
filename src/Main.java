92418ebaed1eb9a22cfa3f153a7046acb86ea8b6
Roman Kuzmin <ghelatin@mail.ru>
2024-05-25 01:28:22 +0300

    HomeWork ForNext Complete

--- src/Main.java
+++ src/Main.java
@@ -46,46 +46,28 @@ public class Main {
 
         System.out.println("Задача 8");
 
-
         int depositBank = 0;
 
-
         for (int i = 1; i <= 12; i += 1) {
-
             depositBank += 29000;
             System.out.printf("Месяц %s, сумма накоплений равна %s рублей\r\n", i, depositBank);
         }
 
-
         System.out.println("Задача 9");
 
-        // Поскольку первоначального вклада на депозит нет, будем считать что условия банка позволяют
-        // вносить его ежемесячно без штрафа.
-        // Для расчета доходности от простых процентов
-        // обычно используется следующая формула:
-        // (Сумма вклада х Годовая ставка х (Количество дней вклада / Количество дней в году)) / 100
-
         depositBank = 0;
-        int depositPercent = 12;
-        int dayInYear = 365;
-        int depositPeriod = 365;
-        int depositPercentAmount;
-
+        int deposit = 29000;
+        int percent = 12;
 
         for (int i = 1; i <= 12; i += 1) {
-
-
-            depositPercentAmount = (depositBank * depositPercent * (depositPeriod / dayInYear)) / 100;
-            int depositTotal = depositBank + depositPercentAmount;
-
-            depositBank += 29000;
-            System.out.printf("Месяц %s, сумма накоплений равна %s рублей\r\n", i, depositTotal);
+            float depositPercent = depositBank * ( (float) percent / 100);
+            depositBank += (int) (deposit + depositPercent);
+            System.out.printf("Месяц %s, сумма накоплений равна %s рублей\r\n", i, depositBank);
         }
 
         System.out.println("Задача 10");
 
         for (int i = 1; i <= 10; i++) {
-
             int multiTotal = 2 * i;
             System.out.println("2*" + i + "=" + multiTotal);
         }
